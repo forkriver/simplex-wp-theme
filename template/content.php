b00e9e97363fdafbd29fffce37ee369f80376636
Patrick Johanneson <pjohanneson@gmail.com>
2016-05-03 22:36:44 -0500

    Continued development

--- template/content.php
+++ template/content.php
@@ -6,7 +6,9 @@ if ( have_posts() ) {
 		the_post();
 		the_date( get_option( 'date_format' ), '<div class="simplex-date">', '</div> <!-- .simplex-date -->' . PHP_EOL );
 		the_title( '<h1>', '</h1>' . PHP_EOL );
-		the_content();
+		the_excerpt();
+		echo( '<p class="more-link"><a href="' . get_permalink() . '">Read <em>' . get_the_title() . '</em>&raquo;</a></p>' . PHP_EOL );
 	}
 	echo( '</div> <!-- .page-content -->' . PHP_EOL );
+		
 }
